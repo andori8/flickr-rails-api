e8ab08a119cca367b8f8ead3a16101e46b903a4e
andori8 <falcsmike7@gmail.com>
2019-01-23 20:33:29 -0500

    rendered json for search database

--- app/controllers/api/v1/search_photos_controller.rb
+++ app/controllers/api/v1/search_photos_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::SearchPhotosController < ApplicationController
   def index
     search = search_params
-    @photos = RestClient::Request.execute(method: "get",
-    url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + ENV['API_KEY'] + "&text=#{search}&extras=url_m&format=json&nojsoncallback=1")
+    @photos = Photo.search_photos(search)
     render json: @photos
   end
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -32,4 +32,26 @@ class Photo < ApplicationRecord
     match_photo.save
     return match_photo
   end
+
+  def self.search_photos(search)
+    @photos = RestClient::Request.execute(method: "get",
+      url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + ENV['API_KEY'] + "&text=#{search}&extras=url_m&format=json&nojsoncallback=1")
+    @info = JSON.parse(@photos)["photos"]["photo"]
+    @filtered_photos = @info.select{ |photo| photo["title"].include?(search) }
+    @filtered_photos.each do |photo|
+      check_photo = self.find_by(photo_id: photo["id"])
+      if !check_photo
+        new_photo = self.new do |key|
+          key.photo_id = photo["id"]
+          key.title = photo["title"]
+          key.url = photo["url_m"]
+          key.owner = photo["owner"]
+          key.server = photo["server"]
+          key.farm = photo["farm"]
+          key.secret = photo["secret"]
+        end
+        new_photo.save
+      end
+    end
+  end
 end
